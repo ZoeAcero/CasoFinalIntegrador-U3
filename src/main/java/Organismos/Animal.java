960a3a45c9013906da189a515f6952fed46a4318
ZoeAcero <zar.amazona@gmail.com>
2024-03-17 20:06:57 +0100

    a

--- src/main/java/Organismos/Animal.java
+++ src/main/java/Organismos/Animal.java
@@ -4,4 +4,11 @@ public class Animal extends Organismo{
     private int cantidadPatas;
     private int cantidadOjos;
     private int cantidadDientes;
+
+    public Animal(int salud, int edad, boolean estadoReproductivo, int cantidadPatas, int cantidadOjos, int cantidadDientes) {
+        super(salud, edad, estadoReproductivo);
+        this.cantidadPatas = cantidadPatas;
+        this.cantidadOjos = cantidadOjos;
+        this.cantidadDientes = cantidadDientes;
+    }
 }
