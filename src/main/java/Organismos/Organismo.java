2f879ae111ffcd38388b8f8ebba9310bb194c41d
ZoeAcero <zar.amazona@gmail.com>
2024-03-17 19:07:40 +0100

    a

--- src/main/java/Organismos/Organismo.java
+++ src/main/java/Organismos/Organismo.java
@@ -2,5 +2,18 @@ package Organismos;
 
 public class Organismo {
     private Posicion posicion;
+    private int salud;
+    private int edad;
+    private boolean estadoReproductivo;
+
+
+    public Organismo(Posicion posicion, int salud, int edad, boolean estadoReproductivo) {
+        this.posicion = posicion;
+        this.salud = salud;
+        this.edad = edad;
+        this.estadoReproductivo = estadoReproductivo;
+    }
+
+    // Metodos getters y setters
     
 }
