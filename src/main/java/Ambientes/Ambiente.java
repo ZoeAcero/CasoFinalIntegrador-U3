5df329e385b5b1fc67fee3444c53a0ef5374ef79
ZoeAcero <zar.amazona@gmail.com>
2024-03-17 20:20:00 +0100

    a

--- src/main/java/Ambientes/Ambiente.java
+++ src/main/java/Ambientes/Ambiente.java
@@ -8,6 +8,16 @@ public class Ambiente {
     private Recursos recursos;
     private Clima clima;
 
+   public Ambiente(int temperatura, int humedad, int cantidadLuz, Terreno terreno, Recursos recursos, Clima clima) {
+        this.temperatura = temperatura;
+        this.humedad = humedad;
+        this.cantidadLuz = cantidadLuz;
+        this.terreno = terreno;
+        this.recursos = recursos;
+        this.clima = clima;
+   }
+
+    // Metodos getters y setters
     
 
 
