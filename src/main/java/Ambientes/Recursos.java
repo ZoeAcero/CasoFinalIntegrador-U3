306f78b4b78dbe0e17c89aa173998ade70e82c92
ZoeAcero <zar.amazona@gmail.com>
2024-03-19 09:08:15 +0100

    a

--- src/main/java/Ambientes/Recursos.java
+++ src/main/java/Ambientes/Recursos.java
@@ -4,7 +4,12 @@ public class Recursos {
     private int agua;
     private int luz;
     private int nutrientes;
-    
+
+    public Recursos(int agua, int luz, int nutrientes) {
+        this.agua = agua;
+        this.luz = luz;
+        this.nutrientes = nutrientes;
+    }
 
 
 
