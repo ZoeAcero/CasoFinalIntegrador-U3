ebeaf0dfbe80338ef5f1d7828db8840d58fcd11e
ZoeAcero <zar.amazona@gmail.com>
2024-03-19 10:20:52 +0100

    a

--- src/main/java/Simulador_Dinamicas_Poblacionales/Cambio_Climatico.java
+++ src/main/java/Simulador_Dinamicas_Poblacionales/Cambio_Climatico.java
@@ -4,4 +4,12 @@ import java.util.List;
 import java.util.Random;
 
 public class Cambio_Climatico extends Eventos_Aleatorios {
+    @Override
+    public void eventoAleatorio(List<Organismo> organismos) {
+        Random random = new Random();
+        for (Organismo organismo : organismos) {
+            boolean estadoReproductivo = random.nextBoolean(); // Estado reproductivo aleatorio
+            organismo.setEstadoReproductivo(estadoReproductivo);
+        }
+    }
 }
