788671bcd186811f4b30c500705eb79fd134f769
ZoeAcero <zar.amazona@gmail.com>
2024-03-19 10:19:08 +0100

    s

---
+++ src/main/java/Simulador_Dinamicas_Poblacionales/Cambio_Climatico.java
@@ -0,0 +1,7 @@
+package Simulador_Dinamicas_Poblacionales;
+import Modelo_Entidades.Ambientes.Organismos.Organismo;
+import java.util.List;
+import java.util.Random;
+
+public class Cambio_Climatico extends Eventos_Aleatorios {
+}
