a6ded2936c82bbd0185fe8daeaeab8659663837b
ZoeAcero <zar.amazona@gmail.com>
2024-03-19 11:36:06 +0100

    a

--- src/main/java/Gestion_Usuarios_Simulaciones/SimulacionConfiguracion.java
+++ src/main/java/Gestion_Usuarios_Simulaciones/SimulacionConfiguracion.java
@@ -5,4 +5,12 @@ public class SimulacionConfiguracion {
     private double tasaCrecimiento;
     private int numeroIteraciones;
 
+    public SimulacionConfiguracion(int tamañoPoblacion, double tasaCrecimiento, int numeroIteraciones) {
+        this.tamañoPoblacion = tamañoPoblacion;
+        this.tasaCrecimiento = tasaCrecimiento;
+        this.numeroIteraciones = numeroIteraciones;
+    }
+
+    // Getters and Setters
+
 }
