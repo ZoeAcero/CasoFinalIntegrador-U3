dd6ee865ec9ae6fc6938302cfffc98591c55437d
ZoeAcero <zar.amazona@gmail.com>
2024-03-19 12:06:35 +0100

    a

--- src/main/java/Analisis_Avanzado/ResolucionProblemas.java
+++ src/main/java/Analisis_Avanzado/ResolucionProblemas.java
@@ -13,12 +13,22 @@ public class ResolucionProblemas {
     public boolean buscarEquilibrioEcologico() {
         List<SimulacionResultados> resultados = registro.getResultados();
         for (SimulacionResultados resultado : resultados) {
-            if (resultado.getSaludPromedio() < 0.5 && resultado.getTamañoPromedio() < 0.5) {
+            if (resultado.getSaludPromedio() < 0.5 || resultado.getTamañoPromedio() < 0.5) {
                 return false;
             }
         }
         return false;
     }
 
-    
+    public void evaluarEstrategiasConservacion() {
+        List<SimulacionResultados> resultados = registro.getResultados();
+        for (SimulacionResultados resultado : resultados) {
+            if (resultado.getTotalOrganismos() < 100) {
+                System.out.println("La estrategia de conservación actual no es efectiva. Se necesita una nueva estrategia.");
+                return;
+            }
+        }
+        System.out.println("La estrategia de conservación actual es efectiva.");
+
+    }
 }
