cf9a6cbd09831beb5854c01e1b47dd846b0ceb73
ZoeAcero <zar.amazona@gmail.com>
2024-03-19 11:13:37 +0100

    a

---
+++ src/main/java/Gestion_Usuarios_Simulaciones/SimulacionResultados.java
@@ -0,0 +1,9 @@
+package Gestion_Usuarios_Simulaciones;
+
+public class SimulacionResultados {
+    private double saludPromedio;
+    private double tamañoPromedio;
+    private int totalOrganismos;
+
+    
+}
