ef1425adb5ed01036c1e74826251970fd4f6484a
ZoeAcero <zar.amazona@gmail.com>
2024-03-19 09:29:40 +0100

    a

---
+++ src/main/java/Modelo_Entidades/Ambientes/Interacciones/Simulador.java
@@ -0,0 +1,11 @@
+package Modelo_Entidades.Ambientes.Interacciones;
+
+import Modelo_Entidades.Ambientes.Organismos.Organismo;
+
+import java.util.List;
+
+
+
+public class Simulador {
+    private List<Organismo> organismos;
+}
