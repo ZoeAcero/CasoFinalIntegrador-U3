c3fa08e75d54ac2c64070ca5123fe21d64e18c73
ZoeAcero <zar.amazona@gmail.com>
2024-03-19 17:42:09 +0100

    a

--- src/main/java/Analisis_Avanzado/Integracion_NuevasFunciones.java
+++ src/main/java/Analisis_Avanzado/Integracion_NuevasFunciones.java
@@ -2,4 +2,10 @@ package Analisis_Avanzado;
 
 public class Integracion_NuevasFunciones {
     private InvestigadorModelo investigadorModelo;
+
+    public void setInvestigadorModelo(InvestigadorModelo investigadorModelo) {
+        this.investigadorModelo = investigadorModelo;
+    }
+
+    
 }
