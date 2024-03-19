7149532f8dae2a6d679600ccdd0cb2434c4353e1
ZoeAcero <zar.amazona@gmail.com>
2024-03-19 09:59:07 +0100

    a

--- src/main/java/Simulador_Dinamicas_Poblacionales/Creciemiento_Reproduccion.java
+++ src/main/java/Simulador_Dinamicas_Poblacionales/Creciemiento_Reproduccion.java
@@ -3,5 +3,10 @@ import Modelo_Entidades.Ambientes.Interacciones.Simulador;
 import Modelo_Entidades.Ambientes.Organismos.Organismo;
 
 public class Creciemiento_Reproduccion {
+    private Simulador simulador;
+
+    public Creciemiento_Reproduccion(Simulador simulador) {
+        this.simulador = simulador;
+    }
 
 }
