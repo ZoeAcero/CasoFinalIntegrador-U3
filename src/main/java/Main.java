5378a61cedc790de2ab6f27bfd8e95d47c6ccadb
ZoeAcero <zar.amazona@gmail.com>
2024-03-19 20:18:02 +0100

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,10 @@ import Analisis_Avanzado.*;
 import Gestion_Usuarios_Simulaciones.*;
 import Modelo_Entidades.Ambientes.*;
 import Simulador_Dinamicas_Poblacionales.*;
+import java.util.Scanner;
+import java.util.Random;
+import java.awt.*;
+import javax.swing.*;
 
 public class Main {
 }
