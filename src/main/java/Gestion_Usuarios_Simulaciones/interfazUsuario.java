e209fc10bdf768eba91cead78f96ad50bb82cc6b
ZoeAcero <zar.amazona@gmail.com>
2024-03-19 10:41:30 +0100

    a

---
+++ src/main/java/Gestion_Usuarios_Simulaciones/interfazUsuario.java
@@ -0,0 +1,9 @@
+package Gestion_Usuarios_Simulaciones;
+import javax.swing.*;
+
+public class interfazUsuario {
+    private SimulacionControl control;
+    private SimulacionConfiguracion configuracion;
+
+    
+}
