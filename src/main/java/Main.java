7e7853041e18b842080c2646699c8a09225ae8cb
ZoeAcero <zar.amazona@gmail.com>
2024-03-19 20:31:29 +0100

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import Analisis_Avanzado.*;
 import Gestion_Usuarios_Simulaciones.*;
 import Modelo_Entidades.Ambientes.*;
+import Modelo_Entidades.Ambientes.Ambiente.Ambiente;
+import Modelo_Entidades.Ambientes.Interacciones.Simulador;
 import Simulador_Dinamicas_Poblacionales.*;
 import java.util.Scanner;
 import java.util.Random;
@@ -8,4 +10,8 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Main {
+    public static void main ( String[] args ) {
+        Scanner scanner = new Scanner ( System.in );
+        Ambiente ambiente = new Ambiente ( "Tropical", "Selva", 1000, 50, 50 );
+        Simulador simulador = new Simulador ( 3.0, 80.0 );
 }
