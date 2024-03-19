91e388ac93c48b5bf4c14467bf263de8dfceb654
ZoeAcero <zar.amazona@gmail.com>
2024-03-19 10:18:19 +0100

    a

---
+++ src/main/java/Simulador_Dinamicas_Poblacionales/Enfermedad.java
@@ -0,0 +1,15 @@
+package Simulador_Dinamicas_Poblacionales;
+import Modelo_Entidades.Ambientes.Organismos.Organismo;
+import java.util.List;
+import java.util.Random;
+
+public class Enfermedad extends Eventos_Aleatorios {
+    @Override
+    public void eventoAleatorio(List<Organismo> organismos) {
+        Random random = new Random();
+        for (Organismo organismo : organismos) {
+            int saludPerdida = random.nextInt(20); // Perdida de salud entre 0 y 20
+            organismo.decrementarSalud(saludPerdida);
+        }
+    }
+}
