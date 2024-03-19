f6671c01d76f933c01be986520dc85c5e97e0694
ZoeAcero <zar.amazona@gmail.com>
2024-03-19 10:16:00 +0100

    a

--- src/main/java/Simulador_Dinamicas_Poblacionales/DesastreNatural.java
+++ src/main/java/Simulador_Dinamicas_Poblacionales/DesastreNatural.java
@@ -1,12 +1,17 @@
 package Simulador_Dinamicas_Poblacionales;
 import Modelo_Entidades.Ambientes.Organismos.Organismo;
 import java.util.List;
+import java.util.Random;
 
 public class DesastreNatural extends Eventos_Aleatorios {
     @Override
     public void eventoAleatorio(List<Organismo> organismos) {
+        Random random = new Random();
         for (Organismo organismo : organismos) {
-            organismo.decrementarSalud(10);
+            int saludPerdida = random.nextInt(20); // Perdida de salud entre 0 y 20
+            organismo.decrementarSalud(saludPerdida);
         }
     }
+
+
 }
