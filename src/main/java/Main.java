895ad5a75a7702352296e67b579551f9038ec431
ZoeAcero <zar.amazona@gmail.com>
2024-03-19 20:33:18 +0100

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,4 +14,17 @@ public class Main {
         Scanner scanner = new Scanner ( System.in );
         Ambiente ambiente = new Ambiente ( "Tropical", "Selva", 1000, 50, 50 );
         Simulador simulador = new Simulador ( 3.0, 80.0 );
+
+        boolean salir = false;
+
+        while (!salir) {
+            System.out.println ( "\n--- Menú Principal ---" );
+            System.out.println ( "1. Modelado de Entidades" );
+            System.out.println ( "2. Simulador de Dinámicas Poblacionales" );
+            System.out.println ( "3. Gestión de Usuarios y Simulaciones" );
+            System.out.println ( "4. Análisis Avanzado" );
+            System.out.println ( "5. Salir" );
+            System.out.print ( "Seleccione una opción: " );
+            scanner.nextLine (); // Consumir el salto de línea
+            int opcion = scanner.nextInt();
 }
