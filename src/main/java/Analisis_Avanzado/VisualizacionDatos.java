f874246e48149fffa0ff9e5514a0c2e066b25638
ZoeAcero <zar.amazona@gmail.com>
2024-03-19 12:14:20 +0100

    a

---
+++ src/main/java/Analisis_Avanzado/VisualizacionDatos.java
@@ -0,0 +1,14 @@
+package Analisis_Avanzado;
+
+import Gestion_Usuarios_Simulaciones.Registro_Actividades.RegistroActividades;
+import Gestion_Usuarios_Simulaciones.Registro_Actividades.SimulacionResultados;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import javax.swing.*;
+import java.util.List;
+
+public class VisualizacionDatos {
+}
