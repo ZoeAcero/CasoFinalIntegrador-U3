ed468010fb360c1d8f4daf407990ab7959a3bd81
ZoeAcero <zar.amazona@gmail.com>
2024-03-20 11:27:31 +0100

    a

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,60 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void ejecutarSimulacion() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visualizarResultados() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void integrarNuevasFunciones() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void resolucionproblemas() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void modeladoEntidadesMenu() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void crearNuevaPlanta() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void crearNuevoAnimal() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void simuladorPoblacionalMenu() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void simularCrecimientoYReproduccion() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void verResultadosSimulacion() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void simularEventosAleatorios() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void gestionUsuariosSimulacionesMenu() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void autenticacion() {
+    }
+}
